30c02ae7b402798c93e4cb5f2200a9f31603c9a6
Vivian Gomez <viviangomez0@gmail.com>
2021-11-30 15:52:11 -0500

    commit tree 1fcb802d3e3969c84a0ac13a55806b46956ebd82

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+function submitData(inputName, inputEmail) {
+    fetch('http://loacalhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json'
+        },
+        body: JSON.stringify( {
+            name: inputName,
+            email: inputEmail
+        })
+    })
+    .then(resp => resp.json())
+    .then(json => {
+        const newItemId = json.id;
+        const p = document.createElement('p')
+        p.textContent = newItemId;
+        const body = document.querySelector('body');
+        body.append(p);
+    })
+    .catch(someError => {
+        const p = document.createElement('p')
+        p.textContent = someError.message;
+        const body = document.querySelector('body');
+        body.append(p);
+    })
+}
